b4b506875035a68b5ad92990bcedcff2b8e0bd5f
cule219 <culaficstefan@gmail.com>
2019-07-17 09:36:02 +0200

    updated

--- starter-code/src/components/AddContact.js
+++ starter-code/src/components/AddContact.js
@@ -15,31 +15,36 @@ export default class AddContact extends Component {
             <div>
                 <form onSubmit={this.handleFormSubmit}>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" value={this.state.name} onChange={(e)=>this.handleNameInput(e)}/>
+                    <input type="text" name="name" value={this.state.name} onChange={(e)=>this.handleChange(e)}/>
                     <label htmlFor="pictureUrl">pictureUrl: </label>
-                    <input type="text" name="pictureUrl" value={this.state.pictureUrl} onChange={(e)=>this.handleImgInput(e)}/>
+                    <input type="text" name="pictureUrl" value={this.state.pictureUrl} onChange={(e)=>this.handleChange(e)}/>
                     <label htmlFor="name">Popularity: </label>
-                    <input type="text" step="0.01" name="popularity" value={this.state.popularity} onChange={e=>this.handlePopularityInput(e)}/>    
+                    <input type="text" step="0.01" name="popularity" value={this.state.popularity} onChange={e=>this.handleChange(e)}/>    
                     <input type="submit"value="Submit" />
                 </form>
             </div>
         )
     }
-    handleNameInput = (e) => {
-        this.setState({
-            name: e.target.value
-        })
+    handleChange = (e) => {
+        let {name, value} = e.target;
+        this.setState({[name]: value})
     }
-    handleImgInput = (e) => {
-        this.setState({
-            pictureUrl: e.target.value
-        })
-    }
-    handlePopularityInput = (e) => {
-        this.setState({
-            popularity: e.target.value
-        })
-    }
+
+    // handleNameInput = (e) => {
+    //     this.setState({
+    //         name: e.target.value
+    //     })
+    // }
+    // handleImgInput = (e) => {
+    //     this.setState({
+    //         pictureUrl: e.target.value
+    //     })
+    // }
+    // handlePopularityInput = (e) => {
+    //     this.setState({
+    //         popularity: e.target.value
+    //     })
+    // }
     handleFormSubmit = (e) => {
         e.preventDefault();
         this.props.addContact(this.state);
